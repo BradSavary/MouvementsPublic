d5be5e4505a58155051d09c9c978d64f66181327
Brad Savary <156096844+BradSavary@users.noreply.github.com>
2025-06-13 17:44:17 +0200

    Update EntityRepository.php

--- api/Repository/EntityRepository.php
+++ api/Repository/EntityRepository.php
@@ -21,7 +21,7 @@ abstract class EntityRepository {
 
     protected function __construct(){
         // Modifiez ici vos informations de BDD et de connexion
-        $this->cnx = new PDO("mysql:host=localhost;port=3306;dbname=mouvement", "pma-admin", "pmaadmin");
+        $this->cnx = new PDO("mysql:host=localhost;port=3306;dbname=mouvement", "user", "mdp");
     }
 
     /**
